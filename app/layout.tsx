05c4e712d09a21290062216d305c692d224f1b11
vishwaadeveloper <vishwaadeveloper@gmail.com>
2025-07-12 16:42:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Boss Fitness',
+  description: 'Created with next.js',
+  dev: 'vishwaa',
 }
 
 export default function RootLayout({
